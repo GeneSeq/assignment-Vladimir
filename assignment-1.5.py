9efba58003f3d7d4360a752d6afee5ade9e9590d
GeneSeq <kopat.v.bio@gmail.com>
2016-02-11 00:09:01 +0300

    hw for thersday

--- assignment-1.5.py
+++ assignment-1.5.py
@@ -18,12 +18,34 @@ def my_filter(fn, elements, **kwargs):
     """
     true_results = []
     for element in elements:
-        result = fn(element, **kwargs)
-        if result:
-           true_results.append(result)
+        if fn(element, **kwargs):
+           true_results.append(element)
     return true_results
 
 
 # Task 5
+# Don't look at this, I just started
 
 
+def evaluate_string():
+    operation_dict = {
+        "+": operator.add,
+    }
+    numbers_iterator = iter(numbers)
+    acc = next(numbers_iterator)
+    for num, oper in zip(numbers_iterator, operations):
+        oper_func = operation_dict.get(oper)
+        if not oper_func:
+            raise ValueError("Operation {} is not supported".format(oper))
+        acc = oper_func(acc, num)
+    return acc
+
+
+
+
+
+
+
+def calculate(numbers, operations):
+    if len(numbers) != len(operations) + 1:
+        raise ValueError("msg")
