9f7d699869d48b62fa532fddc39bd950e94eaa03
GeneSeq <kopat.v.bio@gmail.com>
2016-02-21 23:41:09 +0300

    help

--- assignment-4.py
+++ assignment-4.py
@@ -28,8 +28,8 @@ def p_distance(seq1, seq2):
     return mismatches_without_gaps/(mismatches_without_gaps+ident_positions)
 
 
-# Task 3 (in progress)
-def matrix_mul(mx1, mx2):
+# Task 3 (I don't know how to iter by col and row at the same time)
+def my_dot_product(mx1, mx2):
     """
     :type mx1: list
     :param mx1:
@@ -41,9 +41,22 @@ def matrix_mul(mx1, mx2):
         raise ValueError("Matrix is not matched")
     nrow_mx1, ncol_mx1 = len(mx1), len(mx1[0])
     nrow_mx2, ncol_mx2 = len(mx2), len(mx2[0])
-    multipl_result = [[None for j in xrange(ncol_mx2)]
-                      for i in xrange(nrow_mx1)]
+    mult_result = [[None for j in xrange(ncol_mx2)] for i in xrange(nrow_mx1)]
     def get_col(mx, j):
         return [row[j] for row in mx]
+
+    for j in xrange(ncol_mx2):
+        get_col(mx2, j)
+        for i in mx1[]:
+
+            # get_col(mx2, j) * i for _ in zip(get_col(mx2, j), i)
+
+
+# I need cycle:
+# 1. get col from mx2 by get_col
+# 2. zip (or something else) row and col (row by iteration by index)
+# 3. multiply all pairs
+# 4. summ results
+# 5. put into [multiple_result]
 
 
