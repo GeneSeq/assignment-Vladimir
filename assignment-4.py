eb479b245ff9ae7127e6e95156e94f2dba788efc
GeneSeq <kopat.v.bio@gmail.com>
2016-02-21 21:22:17 +0300

    lessons

--- assignment-4.py
+++ assignment-4.py
@@ -21,7 +21,9 @@ def p_distance(seq1, seq2):
     """
     if len(seq1) != len(seq2):
         raise ValueError("")
-    mismatches_without_gaps = sum(char1 != char2 and char1 != "-" and char2 != "-" for char1, char2 in izip(seq1, seq2))
+    mismatches_without_gaps = sum(char1 != char2 and char1 != "-"
+                                  and char2 != "-" for char1, char2
+                                  in izip(seq1, seq2))
     ident_positions = sum(char1 == char2 for char1, char2 in zip(seq1, seq2))
     return mismatches_without_gaps/(mismatches_without_gaps+ident_positions)
 
