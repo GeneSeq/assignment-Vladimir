95f97a77e909dcbcc1540c09e677804da94ba517
GeneSeq <kopat.v.bio@gmail.com>
2016-02-28 22:13:15 +0300

    hw 5, tasks 1, 3. ready only 3

---
+++ assignment-5.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+
+"""
+hw 5, tasks 1, 3
+"""
+
+
+from __future__ import  division, print_function
+
+
+# Task 1
+
+
+# Task 3
+"""
+sum((Fi, (i=0, n)) = Fn+2 - 1
+1. n = 0. F0 = F0+2 - 1 = F0 + F1 - 1 --> F1 = 1
+The first two elements are 1, so all is OK.
+2. next step (n = 1). Fn + Fn-1 = Fn+2 - 1 = Fn + Fn+1 - 1 = Fn + Fn + Fn-1 -1 -->
+--> 0 = Fn - 1 --> Fn = 1
+"""
